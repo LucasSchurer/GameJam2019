7eb1900351d7373e76d68d5e22096285226df86f
Lucas <lschurer@inf.ufsm.br>
2019-11-10 10:53:54 -0300

    Update game
    Creating lamp object

---
+++ GameJam2019/Assets/Scripts/Objects/Lamp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    private bool isOn = true;
+
+    private SpriteRenderer spriteRenderer;
+    private Light2D light2D;
+
+    public Sprite lampOn;
+    public Sprite lampOff;
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        light2D = GetComponent<Light2D>();
+    }
+
+    void Change()
+    {
+        isOn ^= true;
+
+        spriteRenderer.sprite = isOn ? lampOn : lampOff;
+        light2D.enabled = isOn;
+    }
+}
