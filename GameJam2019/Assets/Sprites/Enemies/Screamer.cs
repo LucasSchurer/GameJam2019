cbbf3ea24658ebefcf563340df9aabd354d06121
Lucas <lschurer@inf.ufsm.br>
2019-11-09 23:32:20 -0300

    Update game
    Tilemap done

---
+++ GameJam2019/Assets/Sprites/Enemies/Screamer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screamer : MonoBehaviour
+{
+    public PlayerInfo playerInfo;
+
+    public float screamCooldown;
+    public float screamCooldownLeft;
+    public float screamRadius;
+    public LayerMask screamMask;
+    public LayerMask soundProofingMask;
+
+    void Update()
+    {
+        Scream();
+    }
+
+    public void Scream()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), screamRadius, screamMask);
+
+        if (collider)
+        {
+            if (!Physics2D.Linecast(transform.position, collider.transform.position, soundProofingMask))
+                playerInfo.Kill();
+        }
+            
+        
+    }
+}
