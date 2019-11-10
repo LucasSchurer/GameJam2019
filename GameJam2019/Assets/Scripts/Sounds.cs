25258d2845ea58a058b284f37519b25cd88b0826
artursendtko <37150919+artursendtko@users.noreply.github.com>
2019-11-10 14:54:35 -0300

    Add files via upload

---
+++ GameJam2019/Assets/Scripts/Sounds.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    public AudioSource source;
+}
+
+//FindObjectOfType.<AudioManager>().Play("Nome do som");
+//Acha o som com o nome e o executa.
